5ab9afa28ca205bf30069214a02bd7be475e1f69
Johan Reitan <johan.reitan@gmail.com>
2020-05-18 13:11:39 +0200

    add cursor and selection

--- flutter_hyttebok/lib/presentation/details_page.dart
+++ flutter_hyttebok/lib/presentation/details_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hyttebok/models/trip.dart';
+import 'package:flutter_hyttebok/shared/custom_cursor.dart';
 
 class DetailsPage extends StatelessWidget {
   final Trip trip;
@@ -8,6 +9,10 @@ class DetailsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text(trip?.name ?? 'Velg en tur'));
+    return Center(
+        child: CustomCursor(
+      cursorStyle: CustomCursor.text,
+      child: SelectableText(trip?.name ?? 'Velg en tur'),
+    ));
   }
 }
--- flutter_hyttebok/lib/presentation/trips_list.dart
+++ flutter_hyttebok/lib/presentation/trips_list.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hyttebok/models/trip.dart';
+import 'package:flutter_hyttebok/shared/custom_cursor.dart';
 
 class TripsList extends StatelessWidget {
   final List<Trip> trips;
@@ -18,11 +20,22 @@ class TripsList extends StatelessWidget {
     return Scrollbar(
       child: ListView.builder(
         itemCount: trips.length,
-        itemBuilder: (context, index) => ListTile(
-          title: Text(trips[index].name),
-          selected: selectedTrip == trips[index],
-          onTap: () => onOpenTrip(trips[index]),
-        ),
+        itemBuilder: (context, index) {
+          var listTile = ListTile(
+            title: Text(trips[index].name),
+            selected: selectedTrip == trips[index],
+            onTap: () => onOpenTrip(trips[index]),
+          );
+          return CustomCursor(
+            child: selectedTrip == trips[index]
+                ? Container(
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).selectedRowColor),
+                    child: listTile,
+                  )
+                : listTile,
+          );
+        },
       ),
     );
   }
